dbdacf253462c507976805d0161d88f4367cba89
zouliping <zouliping007@163.com>
2013-04-08 21:48:27 +0800

    NO.1042 still WA

--- 1043.cpp
+++ 1043.cpp
@@ -3,7 +3,7 @@
 
 using namespace std;
 
-string months[19] = {"pop", "no", "zip", "zotz", "tzec", "xul", "yoxkin", "mol", "chen", "yax", "zac", "ceh", "mac", "kankin", "muan", "pax", "koyab", "cumhu"};
+string months[19] = {"pop", "no", "zip", "zotz", "tzec", "xul", "yoxkin", "mol", "chen", "yax", "zac", "ceh", "mac", "kankin", "muan", "pax", "koyab", "cumhu","uayet"};
 string days[20] = {"imix", "ik", "akbal", "kan", "chicchan", "cimi", "manik", "lamat", "muluk", "ok", "chuen", "eb", "ben", "ix", "mem", "cib", "caban", "eznab", "canac?", "ahau"};
 
 int main(void)
@@ -13,24 +13,31 @@ int main(void)
 	cout<<num<<endl;
 	while(num--)
 	{
-		int i,day,year,month = 20;
+		int i,day,year;
 		int dayCount = 0;
 		string monthName;
 		cin>>day;
 		cin.get();
 		cin>>monthName>>year;
 
-		if(monthName == "cumhu")
-			month = 5;
+		dayCount = day + year * 365;
 		for(i=0;i<19;i++)
+		{
 			if(monthName == months[i])
+			{
+				dayCount += i*20;
 				break;
-		dayCount = year * 365 + i * month + day;
-
+			}
+		}
+			
 		int tYear,tMonth,tDay;
-		tYear = dayCount / 260;
-		tMonth = (dayCount % 260) % 13 + 1;
-		tDay = (dayCount % 260) % 20;
+		if(dayCount % 260 != 0)
+			tYear = dayCount / 260 - 1;
+		else
+			tYear = dayCount / 260;
+		dayCount %= 260;
+		tMonth = dayCount % 13 + 1;
+		tDay = dayCount % 20;
 		cout<<tMonth<<" "<<days[tDay]<<" "<<tYear<<endl;
 	}
 	return 0;
